bb5702d0de01cf9057998480a6c17b2952745a43
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2017-07-31 02:49:00 +0900

    create context

---
+++ src/MilkSharp.Core/MilkContext.cs
@@ -0,0 +1,14 @@
+﻿namespace MilkSharp.Core
+{
+    public class MilkContext
+    {
+        internal string ApiKey { get; private set; }
+        internal string SharedSecret { get; private set; }
+
+        public MilkContext(string apiKey, string sharedSecret)
+        {
+            this.ApiKey = apiKey;
+            this.SharedSecret = sharedSecret;
+        }
+    }
+}
---
+++ test/MilkSharp.Test/AuthorizationTest.cs
@@ -0,0 +1,21 @@
+﻿using MilkSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MilkSharp.Test
+{
+    public class AuthorizationTest
+    {
+        [Fact]
+        public void CreateMilkContext()
+        {
+            var context = new MilkContext(apiKey: "api key", sharedSecret: "shared secret");
+
+            Assert.Equal("api key", context.ApiKey);
+            Assert.Equal("shared secret", context.SharedSecret);
+        }
+
+    }
+}
