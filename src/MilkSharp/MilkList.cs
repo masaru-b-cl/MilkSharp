a089a90a1ad3663c2f3e6fad58c1b0cf72dbdb02
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2017-10-15 23:22:28 +0900

    get all list using `rtm.lists.getList API`

---
+++ src/MilkSharp/MilkList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace MilkSharp
+{
+    public class MilkList
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public static MilkList Parse(XElement list)
+        {
+            return new MilkList
+            {
+                Id = int.Parse(list.Attribute("id").Value),
+                Name = list.Attribute("name").Value,
+            };
+        }
+    }
+}
---
+++ test/MilkSharp.Test/MilkListsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Reactive.Linq;
+using Xunit;
+using Moq;
+using System.Threading.Tasks;
+
+namespace MilkSharp.Test
+{
+    public class MilkListsTest
+    {
+        [Fact]
+        public void GetListTestForAllList()
+        {
+            var milkCoreClientMock = new Mock<IMilkCoreClient>();
+
+            milkCoreClientMock
+                .Setup(client => client.Invoke(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                .Callback<string, IDictionary<string, string>>((method, parameters) =>
+                {
+                    Assert.Equal("rtm.lists.getList", method);
+                })
+                .Returns(() => Task.FromResult<(string, MilkFailureResponse)>((
+                    @"
+                        <rsp stat=""ok"">
+                            <lists>
+                              <list id=""100653"" name=""Inbox""
+                                   deleted=""0"" locked=""1"" archived=""0"" position=""-1"" smart=""0"" />
+                            </lists>
+                        </rsp>
+                        ",
+                    null)));
+            var milkCoreClient = milkCoreClientMock.Object;
+
+            var lists = new MilkLists(milkCoreClient);
+
+            IObservable<MilkList> listObservable = lists.GetList();
+            listObservable.Subscribe(
+                list =>
+                {
+                    Assert.Equal(100653, list.Id);
+                    Assert.Equal("Inbox", list.Name);
+                });
+        }
+    }
+}
