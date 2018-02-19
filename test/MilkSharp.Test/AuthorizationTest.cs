f40a0239478874f9b041853542e112d539fb8331
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2018-02-20 00:04:01 +0900

    remove test class for `MilkContext` class
    `MilkContext` class has tested by other test class.

--- test/MilkSharp.Test/AuthorizationTest.cs
+++ test/MilkSharp.Test/AuthorizationTest.cs
@@ -8,29 +8,6 @@ namespace MilkSharp.Test
 {
     public class AuthorizationTest
     {
-        public class MilkContextTest
-        {
-            [Fact]
-            public void CreateMilkContext()
-            {
-                var context = new MilkContext(apiKey: "api key", sharedSecret: "shared secret");
-
-                Assert.Equal("api key", context.ApiKey);
-                Assert.Equal("shared secret", context.SharedSecret);
-            }
-
-            [Fact]
-            public void SetToken()
-            {
-                var context = new MilkContext(apiKey: "api key", sharedSecret: "shared secret");
-
-                context.AuthToken = new MilkAuthToken();
-
-                Assert.NotNull(context.AuthToken);
-            }
-
-        }
-
         public class MilkAuthorizerTest
         {
             [Fact]
